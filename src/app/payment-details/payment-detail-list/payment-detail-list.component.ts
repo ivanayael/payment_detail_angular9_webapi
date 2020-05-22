1493b1cb392ff89d7b48c2ac1731ee859547c666
Ivana Yael Curra <ivanayael@hotmail.com>
2020-05-22 15:29:07 -0300

    first commit

---
+++ src/app/payment-details/payment-detail-list/payment-detail-list.component.ts
@@ -0,0 +1,39 @@
+import { PaymentDetails } from './../../shared/payment-detail.model';
+import { PaymentDetailsService } from './../../shared/payment-detail.service';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-payment-detail-list',
+  templateUrl: './payment-detail-list.component.html',
+  styles: []
+})
+export class PaymentDetailListComponent implements OnInit {
+
+  constructor(public service: PaymentDetailsService,
+    public toastr: ToastrService) { }
+
+  ngOnInit() {
+    this.service.refreshList();
+  }
+
+  populateForm(pd: PaymentDetails) {
+    this.service.formData = Object.assign({}, pd);
+  }
+
+  onDelete(PMId) {
+    if (confirm('Are you sure to delete this record ?')) {
+      this.service.deletePaymentDetail(PMId)
+        .subscribe(res => {
+          debugger;
+          this.service.refreshList();
+          this.toastr.warning('Deleted successfully', 'Payment Detail Register');
+        },
+          err => {
+            debugger;
+            console.log(err);
+          })
+    }
+  }
+
+}
